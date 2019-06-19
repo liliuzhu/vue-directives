dcb4bdeaa9578a11b4d628acb64dfb56fcfff709
李留住 <liliuzhu@renrenche.com>
2019-06-19 20:41:43 +0800

    feat: 添加出版命令

---
+++ publish.sh
@@ -0,0 +1,24 @@
+set -e
+echo "[vue-directives]Enter publish version: "
+read VERSION
+
+read -p "Publishing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Publishing $VERSION ..."
+
+  # build
+  git checkout master
+  VERSION=$VERSION npm run build
+
+  # commit
+  git add -A
+  git commit -m "[build] $VERSION"
+  npm version $VERSION --message "[publish] $VERSION"
+
+  # publish
+  git push
+  git tag -a v$VERSION
+  npm publish
+fi
