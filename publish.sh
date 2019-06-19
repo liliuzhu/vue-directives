bcd4b56550fbfef690a36ababf28133c492a1bea
liliuzhu <liliuzhu1992@163.com>
2019-06-20 02:51:41 +0800

    feat: npm publish 测试

--- publish.sh
+++ publish.sh
@@ -1,24 +1,15 @@
 set -e
-echo "[vue-directives]Enter publish version: "
-read VERSION
 
-read -p "Publishing $VERSION - are you sure? (y/n)" -n 1 -r
+read -p "Publishing - are you sure? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  echo "Publishing $VERSION ..."
+  echo "Publishing ..."
 
   # build
-  git checkout master
-  VERSION=$VERSION npm run build
-
-  # commit
-  git add -A
-  git commit -m "[build] $VERSION"
-  # npm version $VERSION --message "[publish] $VERSION"
+  #git checkout master
+  npm run build
 
   # publish
-  git push
-  git tag -a v$VERSION -m "v$VERSION"
-  npm publish
+  npm publish --access public
 fi
